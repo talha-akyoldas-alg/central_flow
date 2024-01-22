9231b53872930893a66f784b2b968c2d9feaef5c
talha-akyoldas-alg <talha.akyoldas@1000.com.tr>
2024-01-23 01:59:40 +0300

    first commit

---
+++ newman.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# newman.sh
+
+# Assign passed arguments to variables.....
+BUILD_STATUS="$1"
+WORKFLOW="$2"
+BRANCH="$3"
+REPOSITORY="$4"
+ARTIFACT="$5"
+BINBINAPIGW_COLLECTION_ID="$6"
+BINBINAPIGW_TEST_ENVIRONMENT_ID="$7"
+BINBINAPIGW_WORKSPACE_ID="$8"
+BINBINAPIGW_API_TOKEN="$9"
+TOMP_COLLECTION_ID="$10"
+TOMP_TEST_ENVIRONMENT_ID="$11"
+TOMP_WORKSPACE_ID="$12"
+TOMP_API_TOKEN="$13"
+
+
+if [[ "$BUILD_STATUS" == "cancelled" || "$BUILD_STATUS" == "skipped" ]]; then
+  echo "Workflow skipped / cancelled"
+  exit 1
+fi
+
+TITLE="${REPOSITORY##*/}"
+
+# Convert to lowercase
+WORKFLOW_LOWERCASE=$(echo "$WORKFLOW" | tr '[:upper:]' '[:lower:]')
+echo $WORKFLOW_LOWERCASE
+REPOSITORY_LOWERCASE=$(echo "$TITLE" | tr '[:upper:]' '[:lower:]')
+echo $REPOSITORY_LOWERCASE
+
+
+# Dynamically determine which Google Chat URL to use
+if [[ "$WORKFLOW_LOWERCASE" == *"test"* && "$REPOSITORY_LOWERCASE" == *"binbin-api-gateway"* ]]; then
+    COLLECTION_ID="$BINBINAPIGW_COLLECTION_ID"
+    ENVIRONMENT_ID="$BINBINAPIGW_TEST_ENVIRONMENT_ID"
+    POSTMAN_WORKSPACE_ID="$BINBINAPIGW_WORKSPACE_ID"
+    POSTMAN_API_TOKEN="$BINBINAPIGW_API_TOKEN"
+elif [[ "$WORKFLOW_LOWERCASE" == *"test"* && "$REPOSITORY_LOWERCASE" == *"algoritma-tomp-api"* ]]; then
+    COLLECTION_ID="$TOMP_COLLECTION_ID"
+    ENVIRONMENT_ID="$TOMP_TEST_ENVIRONMENT_ID"
+    POSTMAN_WORKSPACE_ID="$TOMP_WORKSPACE_ID"
+    POSTMAN_API_TOKEN="$TOMP_API_TOKEN"
+fi
+
+
+
+newman run https://api.getpostman.com/collections/$COLLECTION_ID\?apikey\=$POSTMAN_API_TOKEN -e https://api.getpostman.com/environments/$ENVIRONMENT_ID\?apikey\=$POSTMAN_API_TOKEN -r cli,json,postman-cloud,htmlextra --reporter-json-export newman_json_report.json --reporter-postman-cloud-apiKey $POSTMAN_API_TOKEN --reporter-postman-cloud-workspaceId $POSTMAN_WORKSPACE_ID --reporter-htmlextra-export testResults/htmlreport.html | tee newman_terminal_output.txt
+
+
+#Extracting Postman Cloud Result URL from Newman Terminal Output
+url=$(grep -o 'https://go.postman.co/workspace/[^ ]*' newman_terminal_output.txt | head -1)
+echo "$url" > newman_terminal_output.txt
+echo "URL extracted and saved to newman_terminal_output.txt"
+urlFromFile=$(cat newman_terminal_output.txt | tr -d '\n')
+echo "POSTMAN_CLOUD_REPORT_BUTTON_LINK=$urlFromFile" >> $GITHUB_ENV
+
+#Make Directory for Newman HTML reports
+mkdir -p testResults
---
+++ notifier.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+# send_notification.sh
+
+WORKFLOW="$1"
+REPOSITORY="$2"
+ARTIFACT="$3"
+GOOGLE_CHAT_BINBIN_TEST_URL="$4"
+GOOGLE_CHAT_BINBIN_PROD_URL="$5"
+GOOGLE_CHAT_ALGORITMA_TEST_URL="$6"
+GOOGLE_CHAT_ALGORITMA_PROD_URL="$7"
+
+TIME=$(TZ="Europe/Istanbul" date '+%Y-%m-%d %H:%M:%S')
+
+
+# Set the title and subtitle based on job type and build status
+TITLE="${REPOSITORY##*/} Regression Test Results" 
+
+
+# Convert to lowercase
+WORKFLOW_LOWERCASE=$(echo "$WORKFLOW" | tr '[:upper:]' '[:lower:]')
+echo "WORKFLOW: " $WORKFLOW_LOWERCASE
+REPOSITORY_LOWERCASE=$(echo "$TITLE" | tr '[:upper:]' '[:lower:]')
+echo "REPOSITORY: " $REPOSITORY
+
+
+# Dynamically determine which Google Chat URL to use
+if [[ "$WORKFLOW_LOWERCASE" == *"test"* && "$REPOSITORY_LOWERCASE" != *"algoritma"* ]]; then
+    GOOGLE_CHAT_URL="$GOOGLE_CHAT_BINBIN_TEST_URL"
+elif [[ "$WORKFLOW_LOWERCASE" == *"rollback"* && "$REPOSITORY_LOWERCASE" != *"algoritma"* ]]; then
+    GOOGLE_CHAT_URL="$GOOGLE_CHAT_BINBIN_PROD_URL"
+elif [[ "$WORKFLOW_LOWERCASE" == *"prod"* && "$REPOSITORY_LOWERCASE" != *"algoritma"* ]]; then
+    GOOGLE_CHAT_URL="$GOOGLE_CHAT_BINBIN_PROD_URL"
+elif [[ "$WORKFLOW_LOWERCASE" == *"test"* && "$REPOSITORY_LOWERCASE" == *"algoritma"* ]]; then
+    GOOGLE_CHAT_URL="$GOOGLE_CHAT_ALGORITMA_TEST_URL"
+elif [[ "$WORKFLOW_LOWERCASE" == *"prod"* && "$REPOSITORY_LOWERCASE" == *"algoritma"* ]]; then
+    GOOGLE_CHAT_URL="$GOOGLE_CHAT_ALGORITMA_PROD_URL"
+fi
+
+
+if [ -z "$GOOGLE_CHAT_URL" ]; then
+  echo "Error: GOOGLE_CHAT_URL is empty"
+  exit 1
+fi
+
+
+# URL for the success and failure icons
+SUCCESS_ICON="https://www.iconsdb.com/icons/preview/green/ok-xxl.png"
+FAILURE_ICON="https://www.iconsdb.com/icons/preview/red/x-mark-3-xxl.png"
+
+# Set subtitle based on assertions failed count
+if [ "$ASSERTIONS_FAILED" -gt 0 ]; then
+  IMAGE_URL="$FAILURE_ICON"
+  SUBTITLE="We have a failed test!"
+else
+  IMAGE_URL="$SUCCESS_ICON"
+  SUBTITLE="All tests succeeded!"
+fi
+
+
+#Seting Button Texts
+POSTMAN_CLOUD_REPORT_BUTTON_TEXT="POSTMAN CLOUD REPORT"
+NEWMAN_HTML_REPORT_BUTTON_TEXT="NEWMAN HTML REPORT"
+
+
+#Set Google Chat Notification Requester Payload
+JSON_PAYLOAD=$(cat << EOM
+{
+    "cards": [
+        {
+            "header": {
+                "title": "$TITLE",
+                "subtitle": "$SUBTITLE",
+                "imageUrl": "$IMAGE_URL"
+            },
+            "sections": [
+                {
+                    "widgets": [
+                        {
+                            "keyValue": {
+                                "topLabel": "Time (Istanbul)",
+                                "content": "$TIME"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Iterations",
+                                "content": "$ITERATIONS_TOTAL executed, $ITERATIONS_FAILED failed"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Requests",
+                                "content": "$REQUESTS_TOTAL executed, $REQUESTS_FAILED failed"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Test-Scripts",
+                                "content": "$TEST_SCRIPTS_TOTAL executed, $TEST_SCRIPTS_FAILED failed"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Prerequest-Scripts",
+                                "content": "$PRE_REQUEST_SCRIPTS_TOTAL executed, $PRE_REQUEST_SCRIPTS_FAILED failed"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Assertions",
+                                "content": "$ASSERTIONS_TOTAL executed, $ASSERTIONS_FAILED failed"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Total Run Duration",
+                                "content": "$RUN_DURATION_TOTAL s"
+                            }
+                        },
+                        {
+                            "keyValue": {
+                                "topLabel": "Average Response Time",
+                                "content": "$RESPONSE_TIME_TOTAL ms [min: $RESPONSE_TIME_MIN ms, max: $RESPONSE_TIME_MAX s]" 
+                            }
+                        },
+                        {
+                            "buttons": [
+                                {
+                                    "textButton": {
+                                        "text": "$POSTMAN_CLOUD_REPORT_BUTTON_TEXT",
+                                        "onClick": {
+                                            "openLink": {
+                                                "url": "$POSTMAN_CLOUD_REPORT_BUTTON_LINK"
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            "buttons": [
+                                {
+                                    "textButton": {
+                                        "text": "$NEWMAN_HTML_REPORT_BUTTON_TEXT",
+                                        "onClick": {
+                                            "openLink": {
+                                                "url": "$NEWMAN_HTML_REPORT_BUTTON_LINK"
+                                            }
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+EOM
+)
+
+
+# Send notification to Google Chat
+curl -H 'Content-Type: application/json' -X POST \
+  "${GOOGLE_CHAT_URL}" \
+  --data "$JSON_PAYLOAD"
